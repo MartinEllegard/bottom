9183d58780cfaf5cf1e51d839e11cdfdf323795a
Martin Ellegård <martin.ellegard@gmail.com>
2024-10-24 13:26:53 +0200

    Updated depricated PanicInfo to use new PanicHookInfo

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ pub mod widgets;
 use std::{
     boxed::Box,
     io::{stderr, stdout, Write},
-    panic::{self, PanicInfo},
+    panic::{self, PanicHookInfo},
     sync::{
         mpsc::{self, Receiver, Sender},
         Arc,
@@ -103,7 +103,7 @@ fn check_if_terminal() {
 
 /// A panic hook to properly restore the terminal in the case of a panic.
 /// Originally based on [spotify-tui's implementation](https://github.com/Rigellute/spotify-tui/blob/master/src/main.rs).
-fn panic_hook(panic_info: &PanicInfo<'_>) {
+fn panic_hook(panic_info: &PanicHookInfo<'_>) {
     let mut stdout = stdout();
 
     let msg = match panic_info.payload().downcast_ref::<&'static str>() {
